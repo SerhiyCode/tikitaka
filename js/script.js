988ce8a3f2083cc8f5a85ac92098de3c0312e61d
Serhiy <seregarachuk20@gmail.com>
2026-01-08 17:48:06 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+document.getElementById('toggleBtn').addEventListener('click', () => {
+    document.getElementById('sidebar').classList.toggle('collapsed');
+    document.querySelector('.content-wrapper').classList.toggle('collapsed');
+}); 
+
