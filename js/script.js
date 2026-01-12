9c76029b815dfa284a9e65ea6e72030354486468
Serhiy <seregarachuk20@gmail.com>
2026-01-12 16:17:46 +0200

    fix style bar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-
 document.getElementById('toggleBtn').addEventListener('click', () => {
     document.getElementById('sidebar').classList.toggle('collapsed');
+    document.querySelector('.page-content-wrapper').classList.toggle('collapsed');
 }); 
-
-
-
