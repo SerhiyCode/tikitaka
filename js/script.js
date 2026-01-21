f6e328787fa259fe3d4515abba0f4b8254a7c54a
Serhiy <seregarachuk20@gmail.com>
2026-01-21 13:27:07 +0200

    change of main content

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ btn.onclick = e => {
     sidebar.classList.toggle('collapsed');
     wrapper.classList.toggle('collapsed');
 };
+
+let scrollerWrap = document.querySelector('.scroller-wrap');
+let backBtn = document.getElementById("blackBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollerWrap.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollerWrap.scrollLeft += evt.deltaY;
+});
+
+backBtn.addEventListener("click", () => {
+    scrollerWrap.scrollBy({left: +-300, behavior: 'smooth'}); 
+});
+
+nextBtn.addEventListener("click", () => {
+    scrollerWrap.scrollBy({left: 320, behavior: 'smooth'}); 
+}); 
+
+
